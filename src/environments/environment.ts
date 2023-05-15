df1e602001a45d6ddf30573ec41b1306dbc27d59
dbolzan <80838100+dbolzan@users.noreply.github.com>
2023-05-15 22:50:45 +0200

    change key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,19 +1,12 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-
+import { firebaseConfig } from "./../../config";
 export const environment = {
   production: false,
-  firebaseConfig: {
-    apiKey: "AIzaSyCAXQcXoxpoQe-tgCzjJLogKQZJsXhnd38",
-    authDomain: "matrimonio-gran.firebaseapp.com",
-    projectId: "matrimonio-gran",
-    storageBucket: "matrimonio-gran.appspot.com",
-    messagingSenderId: "655952904258",
-    appId: "1:655952904258:web:d77efcf2d659b2cab36ddb"
-  }
+  firebaseConfig
 };
- 
+
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
